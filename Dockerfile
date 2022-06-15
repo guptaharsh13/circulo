f3ec5fa6bc45f9e22aeb6a8468494d9831c1c3c0
Harsh Gupta <hg242322@gmail.com>
2022-06-16 01:30:41 +0530

    feat: docker support

--- Dockerfile
+++ Dockerfile
@@ -9,9 +9,6 @@ RUN apk add --update --no-cache --virtual .tmp-build-deps \
 RUN pip install -r /requirements.txt
 RUN apk del .tmp-build-deps
 
-RUN pwd
 RUN mkdir /app
 WORKDIR /app
 COPY . /app
-RUN ls youtube_videos/migrations
-RUN ls
--- youtube_videos/management/commands/wait_for_db.py
+++ youtube_videos/management/commands/wait_for_db.py
@@ -2,22 +2,12 @@ import time
 from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management import BaseCommand
-import os
-import environ
-from pathlib import Path
-
-path = Path(__file__).resolve().parent.parent.parent.parent
-env = environ.Env()
-environ.Env.read_env(env_file=os.path.join(path, ".env"))
 
 
 class Command(BaseCommand):
     """Django command to pause execution until db is available"""
 
     def handle(self, *args, **options):
-        print("\n\n")
-        print(env("db_password"))
-        print("\n\n")
         self.stdout.write('Waiting for database...')
         db_conn = None
         while not db_conn:

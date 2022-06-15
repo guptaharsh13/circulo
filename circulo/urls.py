620159791abf321178d9a0f0f22179ac4717a12b
Harsh Gupta <hg242322@gmail.com>
2022-06-15 20:57:21 +0530

    feat: youtube video routes

--- circulo/urls.py
+++ circulo/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from .views import index
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -22,6 +22,7 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path("", view=index, name="index"),
+    path("youtube-videos", include("youtube_videos.urls")),
 
     path('swagger/', schema_view.with_ui('swagger',
          cache_timeout=0), name='schema-swagger-ui'),
